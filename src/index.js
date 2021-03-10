6b1d9d810edf917e87c1e642bfb63fe8119c976a
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-03-10 20:06:37 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+	if (n<0) {
+		n=0-n;
+	}
+   var str = "";
+  var arr = n.toString().split('');
+  for (let i = arr.length-1; i>=0; i--) {
+    
+     str+=arr[i];
+  }
+  return Number(str);
 }
